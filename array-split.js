f30cfb13421d80e5167f11cd8c737d7856767a73
MaheSajib <mahe.sajib@gmail.com>
2021-02-04 00:46:34 +0600

    finishing with break and continue method

--- array-split.js
+++ array-split.js
@@ -2,8 +2,8 @@ const nums = [1, 2, 3, 4, 5, 6, 7, 8];
 // const part = nums.slice(2, 5);
 
 const removed = nums.splice(2, 5, 77);
-// console.log(removed);
-// console.log(nums);
+console.log(removed);
+console.log(nums);
 
 const together = nums.join(" , ");
 console.log(together);
